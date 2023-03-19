332a598c0615f87111bdff03c5f5ceb33ee27cfd
JoshuaFowler <jfowler8912@gmail.com>
2023-03-19 14:43:12 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = (num1 * num2)
+
+let number = true
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 104
+let num4 = 25
+let mod = (num3 % num4)
+
+let max = Math.max(1,3,5,7,13,20)
